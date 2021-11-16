293004f57b279f4ee3cb3776b93ff02748f17e2c
Mohd Zamri Osman <mzamri27@gmail.com>
2021-11-17 00:36:05 +0800

    Update sentiment_app.py
    adding name, and control flow

--- sentiment_app.py
+++ sentiment_app.py
@@ -18,6 +18,7 @@ st.title("Sentiment Analysis Assessment")
 st.write("Sentiment analysis is the process of detecting positive or negative"
 "sentiment in text. It’s often used by businesses to detect sentiment in social "
 "data, gauge brand reputation, and understand customers.")
+st.write("This app created by zamriosman@ump.edu.my")
 
 st.subheader("Types of Sentiment Analysis")
 st.write("""Sentiment analysis focuses on polarity (positive, negative, neutral)
@@ -29,5 +30,7 @@ st.subheader("Try yourself!")
 mystr = st.text_input("Write any 'Positive', 'Negative' words in this text field")
 
 
-if st.button("Classify"):
+if st.button("Classify") and mystr is not None:
     st.write(getPolarity(mystr))
+else:
+    st.write("Please enter any text")
